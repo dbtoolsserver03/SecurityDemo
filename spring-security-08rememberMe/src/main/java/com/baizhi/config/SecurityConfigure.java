6791ec3e8bcc876cb83b621b66fa99579808783e
dbtoolsserver03 <dbtoolsserver03@gmail.com>
2023-07-25 08:26:14 +0900

    072502

--- spring-security-08rememberMe/src/main/java/com/baizhi/config/SecurityConfigure.java
+++ spring-security-08rememberMe/src/main/java/com/baizhi/config/SecurityConfigure.java
@@ -27,8 +27,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
-import com.baizhi.security.filter.KaptchaFilter;
+import com.baizhi.security.filter.LoginKaptchaFilter;
 
 /**
  * An example of explicitly configuring Spring Security with the defaults.
@@ -49,9 +50,17 @@ public class SecurityConfigure {
 	}
 	
     @Bean
-    public KaptchaFilter kaptchaFilter() throws Exception {
-        KaptchaFilter kaptchaFilter = new KaptchaFilter();
-        return kaptchaFilter;
+    public LoginKaptchaFilter kaptchaFilter() throws Exception {
+    	LoginKaptchaFilter loginFilter = new LoginKaptchaFilter();
+        loginFilter.setFilterProcessesUrl("/doLogin");//指定认证 url
+        loginFilter.setUsernameParameter("uname");//指定接收 用户名 key
+        loginFilter.setPasswordParameter("passwd");//指定接收 密码 key
+        
+        loginFilter.setAuthenticationManager(authenticationManager());    	
+    	
+        loginFilter.setSecurityContextRepository(new HttpSessionSecurityContextRepository());
+
+        return loginFilter;
     }
     
 	@Bean
@@ -67,9 +76,7 @@ public class SecurityConfigure {
 		
 		.formLogin((form) -> form
 				.loginPage("/login.html")
-				.loginProcessingUrl("/doLogin")
-				.usernameParameter("uname")
-				.passwordParameter("passwd")
+
 				//						.successForwardUrl("/home")   //不会跳转到之前请求路径
 				.defaultSuccessUrl("/index.html", true)//如果之前有请求路径，会优先跳转之前请求路径，可以传入第二个参数进行修改。
 				.failureUrl("/login.html")//重定向到登录页面 失败之后redirect跳转
@@ -83,52 +90,10 @@ public class SecurityConfigure {
         // at: 用来某个 filter 替换过滤器链中哪个 filter
         // before: 放在过滤器链中哪个 filter 之前
         // after: 放在过滤器链中那个 filter 之后
-        http.addFilterBefore(kaptchaFilter(), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAt(kaptchaFilter(), UsernamePasswordAuthenticationFilter.class);
    
 		// @formatter:on
 		return http.build();
 	}
 	
-
-	 
-	
-//	
-//    @Autowired
-//    private MyUserDetailService myUserDetailsService;
-//    @Bean
-//    AuthenticationManager  authenticationManager() {
-//    	DaoAuthenticationProvider  authenticationProvider = new DaoAuthenticationProvider ();
-//        authenticationProvider.setUserDetailsService(myUserDetailsService);
-//        ProviderManager pm = new ProviderManager(authenticationProvider);
-//        return pm;
-//    }
-	
-	
-    /**
-     * 调用loadUserByUsername获得UserDetail信息，在AbstractUserDetailsAuthenticationProvider里执行用户状态检查
-     *
-     * @return
-     */
-//    @Bean
-//    public AuthenticationProvider authenticationProvider() {
-//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-//        // DaoAuthenticationProvider 从自定义的 userDetailsService.loadUserByUsername 方法获取UserDetails
-//        authProvider.setUserDetailsService(myUserDetailsService);
-//        // 设置密码编辑器
-//        //authProvider.setPasswordEncoder(passwordEncoder());
-//        return authProvider;
-//    }
- 
-//    /**
-//     * 登录时需要调用AuthenticationManager.authenticate执行一次校验
-//     *
-//     * @param config
-//     * @return
-//     * @throws Exception
-//     */
-//    @Bean
-//    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-//        return config.getAuthenticationManager();
-//    }
-
 }
