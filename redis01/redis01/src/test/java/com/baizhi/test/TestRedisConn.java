9c7f68649bb004648a278633add0d544017773ce
user <user@localhost>
2023-09-06 11:48:29 +0900

    REDIS

---
+++ redis01/redis01/src/test/java/com/baizhi/test/TestRedisConn.java
@@ -0,0 +1,32 @@
+package com.baizhi.test;
+
+import org.junit.Test;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * Created by HIAPAD on 2019/11/13.
+ */
+public class TestRedisConn {
+
+	@Test
+	public void testConn() {
+		//  ps -ef | grep redis
+
+		//创建jedis客户端对象
+		//参数1:主机ip 参数2:端口
+		Jedis jedis = new Jedis("127.0.0.1", 6379);
+		System.out.println(jedis.get("name"));
+		
+		jedis = new Jedis("172.16.158.131", 6379);
+		System.out.println(jedis.get("name"));
+		
+		jedis = new Jedis("172.16.158.128", 6379);
+		System.out.println(jedis.get("name"));
+		//jedis.auth("123");
+		//操作redis 选择一个库 默认也是0号库
+
+		//jedis.select(0);//选择零号库
+
+	}
+}
